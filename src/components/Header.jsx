2955a9e5f324814107d8aa9cfae4353411084e0c
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-06-09 16:22:07 +0530

    feat: Add header to the web-pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="bg-lime-200 ">
+      <div className="flex justify-between items-center p-3">
+        <h1 className="font-bold text-4xl lg:text-4xl flex flex-wrapg">
+          <Link to="/">
+            <button className="hover:opacity-90">
+              <span className="text-lime-500">Food</span>
+              <span className="text-lime-700">Share</span>
+            </button>
+          </Link>
+        </h1>
+        <form className="bg-lime-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-lime-500" />
+        </form>
+        <ul className="flex gap-8">
+          <li className="hidden lg:inline text-lime-700 hover:text-lime-900">
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+          </li>
+          <li className="hidden md:inline text-lime-700 hover:text-lime-900">
+            <Link to="/about">
+              {" "}
+              <button>About</button>
+            </Link>
+          </li>
+          <li className="text-lime-700 hover:text-lime-900">
+            <Link to="/sign-in">
+              {" "}
+              <button>Sign in</button>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
