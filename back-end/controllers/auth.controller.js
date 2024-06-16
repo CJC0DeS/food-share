75506e04728e72ff8af2e26cbb2fedb0ea32a208
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-06-16 16:00:12 +0530

    feat: Handle password and error gracefully

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const auth = async (req, res) => {
-    try{
-        const { username, email, password } = req.body;
-        const newUser = new User({ username, email, password });
-        await newUser.save();
-        res.status(201).json("User created successfully");
-    }
-    catch{
-        res.status(400).json("User not created");
-    }
-
+  try {
+    const { username, email, password } = req.body;
+    const hashedPass = bcryptjs.hashSync(password, 7);
+    const newUser = new User({ username, email, password: hashedPass });
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch(error) {
+    res.status(500).json(error.errmsg);
   }
+};
