047798ce97b00c530934434c97ec801b555b2fe9
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-06-30 01:22:24 +0530

    feat: Setup backend for sign-in

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signUp = async (req, res, next) => {
   try {
@@ -8,6 +10,22 @@ export const signUp = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPass });
     await newUser.save();
     res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({email: email})
+    if(!validUser) return next(errorHandler(404, 'User not found'))
+      const validPass = bcryptjs.compareSync(password, validUser.password)
+    if(!validPass) return next(errorHandler(401, 'Inccorrect password'))
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    // Remove password from validUser
+    const {password: value, ...userWithoutPass} = validUser._doc
+    res.cookie('access_token',token, {httpOnly:true}).status(200).json(userWithoutPass)
   } catch (error) {
     next(error);
   }
