cc41c707d0fb11f89707c8284cee2e5bae27c03d
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-07-02 02:35:06 +0530

    feat: Add sign-with-google func. in backend

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken'
+import { generateRandomPassword } from "../utils/passwordGenerator.js";
 
 export const signUp = async (req, res, next) => {
   try {
@@ -30,3 +31,57 @@ export const signIn = async (req, res, next) => {
     next(error);
   }
 };
+
+export const googleSign = async (req, res, next) => {
+  const { email, displayName, photoURL } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    // If user has already signed up to the portal
+    if (validUser) {
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      // Remove password from validUser
+      const { password: value, ...userWithoutPass } = validUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(userWithoutPass);
+    }
+
+    // If user has not signed up to the portal
+    else {
+      const randomlyGeneratedPass = generateRandomPassword(12);
+      const hashedPassword = bcryptjs.hashSync(randomlyGeneratedPass, 7);
+      const generatedUserName =
+        displayName.split(" ").join("").toLowerCase() +
+        generateRandomPassword(6);
+      while (true) {
+        const validUser = await User.findOne({ username: generatedUserName });
+        if (!validUser) {
+          break;
+        }
+        generatedUserName =
+          displayName.split(" ").join("").toLowerCase() +
+          generateRandomPassword(6);
+      }
+
+      const newUser = new User({
+        username: generatedUserName,
+        email: email,
+        password: hashedPassword,
+        avatar: photoURL,
+      });
+      await newUser.save();
+
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      // Remove password from validUser
+      const { password: value, ...userWithoutPass } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(userWithoutPass);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
--- back-end/routes/auth.routes.js
+++ back-end/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
+import { googleSign, signIn, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
-authRouter.post('/signin',signIn)
+authRouter.post("/signin", signIn);
+authRouter.post("/google-sign", googleSign);
 
 export { authRouter };
