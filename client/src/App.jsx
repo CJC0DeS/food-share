2524256a7b7dd746ea8581395e5fc2ce27fc7729
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-07-02 16:07:02 +0530

    fix: Import Profile-page in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Header from "./components/Header";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
