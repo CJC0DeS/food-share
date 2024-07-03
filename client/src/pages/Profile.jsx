b3ed6dadc08b3f73d2d6c59a1ddf524aeddbf7e9
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-07-04 01:20:38 +0530

    feat: Impl. profile-img upload func using firebase

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Icon from "@mdi/react";
 import { mdiTrashCanOutline } from "@mdi/js";
 import { mdiLogout } from "@mdi/js";
+import { useRef } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [imageUploadPercent, setImageUploadPercent] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(formData);
   const handleChange = (event) => {
     console.log("WASSUP");
+  };
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        setImageUploadPercent(
+          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+        );
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -15,11 +57,58 @@ export default function Profile() {
         Profile
       </h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          hidden
+        />
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar ?? currentUser.avatar}
           alt="profile photo"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-4"
         />
+        <div className="text-sm self-center">
+          {fileUploadError ? (
+            <div>
+              <div className="w-80 bg-gray-200 rounded-full h-2.5 dark:bg-gray-300">
+                <div
+                  className="bg-red-600 h-2.5 rounded-full"
+                  style={{ width: `${imageUploadPercent}%` }}
+                ></div>
+              </div>
+              <span className="text-red-700">
+                ERROR: Image size should not exceed 2 MB
+              </span>
+            </div>
+          ) : imageUploadPercent > 0 && imageUploadPercent < 100 ? (
+            <div>
+              <div className="w-80 bg-gray-200 rounded-full h-2.5 dark:bg-gray-300">
+                <div
+                  className="bg-green-600 h-2.5 rounded-full"
+                  style={{ width: `${imageUploadPercent}%` }}
+                ></div>
+              </div>
+              <span className="text-slate-700">{`Uploading ${imageUploadPercent}%`}</span>
+            </div>
+          ) : imageUploadPercent === 100 ? (
+            <div>
+              <div className="w-80 bg-gray-200 rounded-full h-2.5 dark:bg-gray-300">
+                <div
+                  className="bg-green-600 h-2.5 rounded-full"
+                  style={{ width: "100%" }}
+                ></div>
+              </div>
+              <span className="text-green-700">
+                Image successfully uploaded!
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
         <input
           type="text"
           placeholder="Username"
