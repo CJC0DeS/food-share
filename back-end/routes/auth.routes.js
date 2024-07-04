f72848665c7c9977614deccdadf9a1eac17c8c07
CJEEVCODES <chiranjeev@skylarkdrones.com>
2024-07-05 02:43:55 +0530

    feat: Implement sign-out functionality [BE]

--- back-end/routes/auth.routes.js
+++ back-end/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { googleSign, signIn, signUp } from "../controllers/auth.controller.js";
+import { googleSign, signIn, signUp, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
 authRouter.post("/signin", signIn);
 authRouter.post("/google-sign", googleSign);
+authRouter.get('/signout', signOut)
 
 export { authRouter };
